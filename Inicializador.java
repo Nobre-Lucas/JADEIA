f83194f5264b6d3744007a6359911d7d38aef521
Samuel Figueira Aguiar <50933770+RFLMNaguiar@users.noreply.github.com>
2022-06-25 16:03:03 -0300

    first comiitttt

---
+++ Inicializador.java
@@ -0,0 +1,7 @@
+import jade.core.Agent;
+
+public class Inicializador extends Agent {
+    protected void setup(){
+        System.out.println("Hello world, I am an agent!");
+    }
+}
---
+++ RealocadorDoPalete.java
@@ -0,0 +1,7 @@
+import jade.core.Agent;
+
+public class RealocadorDoPalete extends Agent {
+    protected void setup(){
+        System.out.println("Hello world, I am an agent!");
+    }
+}
